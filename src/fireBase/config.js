f838924380af97bc5658cf767b1aff666c80c4eb
Asish <ashisish1858@gamil.com>
2024-03-06 19:53:50 +0530

    olx done

--- src/fireBase/config.js
+++ src/fireBase/config.js
@@ -2,15 +2,15 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCk_yvZCkiZHOWcmfUYsDej7_wjsMHED3g",
-  authDomain: "olx-clone-8fb3c.firebaseapp.com",
-  projectId: "olx-clone-8fb3c",
-  storageBucket: "olx-clone-8fb3c.appspot.com",
-  messagingSenderId: "521728138319",
-  appId: "1:521728138319:web:65a71a2a4ee20186585e3a",
-  measurementId: "G-X9HH3EW211"
+  apiKey: "AIzaSyDvZlB8UW5NQGPssNKOx_uLRXtpbIQEFVc",
+  authDomain: "olx-react-f719d.firebaseapp.com",
+  projectId: "olx-react-f719d",
+  storageBucket: "olx-react-f719d.appspot.com",
+  messagingSenderId: "832762424947",
+  appId: "1:832762424947:web:9b7ed4ccbbc418a37f4406",
+  measurementId: "G-W1E1ZN0L6Z"
 };
 
 export default firebase.initializeApp(firebaseConfig);
